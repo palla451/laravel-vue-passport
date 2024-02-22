1cb59084f40605b75a96c906a7bfd6605a4f265e
Giovanni D'Apote <gdapote@MacBook-Pro-di-Giovanni.local>
2024-02-22 12:34:36 +0100

    second commit

--- src/guards/authGuard.js
+++ src/guards/authGuard.js
@@ -4,11 +4,9 @@ export default function authGuard(to, from, next) {
 
     const token = localStorage.getItem('token');
     if (token) {
-        debugger;
         // Se l'utente è autenticato, consenti la navigazione
         next();
     } else {
-        debugger;
         // Se l'utente non è autenticato, reindirizzalo alla pagina di login
         next('/login');
     }
--- src/store/auth/authLogin.js
+++ src/store/auth/authLogin.js
@@ -2,17 +2,20 @@ import axios from "axios";
 
 const state= {
     userLogged:null,
-    access_token:null
+    access_token:null,
+    refresh_token:null
 };
 
 const getters= {
     getUserLogged: state => state.userLogged,
-    getAccessToken: state => state.access_token
+    getAccessToken: state => state.access_token,
+    getRefreshToken: state => state.refresh_token
 };
 
 const mutations= {
     setUserLogged: (state,userLogged) => { state.userLogged = userLogged },
     setAccessToken: (state,access_token) => { state.access_token = access_token },
+    setRefreshToken: (state,refresh_token) => { state.refresh_token = refresh_token },
 };
 
 const actions = {
@@ -22,7 +25,8 @@ const actions = {
         console.log(response);
         commit('setUserLogged',response.data.user);
         commit('setAccessToken',response.data.access_token);
-        localStorage.setItem('token',response.data.access_token);
+        localStorage.setItem('access_token',response.data.access_token);
+        localStorage.setItem('refresh_token',response.data.refresh_token);
     }
 };
 
